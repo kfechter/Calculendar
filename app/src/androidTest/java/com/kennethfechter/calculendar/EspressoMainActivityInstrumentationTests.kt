60ee983b1d1e05220f45f9c702ca08ed4b62945c
Kenneth Fechter <kenneth.fechter@gmail.com>
2019-10-01 00:16:00 -0400

    Adding test for about application button on main page

--- app/src/androidTest/java/com/kennethfechter/calculendar/EspressoMainActivityInstrumentationTests.kt
+++ app/src/androidTest/java/com/kennethfechter/calculendar/EspressoMainActivityInstrumentationTests.kt
@@ -1,13 +1,18 @@
 package com.kennethfechter.calculendar
 
+import android.app.Instrumentation
 import androidx.test.espresso.Espresso
-import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.matcher.ComponentNameMatchers.hasClassName
+import androidx.test.espresso.intent.matcher.IntentMatchers
 import androidx.test.espresso.matcher.RootMatchers.isDialog
 import androidx.test.espresso.matcher.RootMatchers.withDecorView
-import androidx.test.espresso.matcher.ViewMatchers
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.rule.ActivityTestRule
+import com.kennethfechter.calculendar.activities.CalculendarAbout
+import org.hamcrest.CoreMatchers
 import org.junit.Rule
 import org.junit.Test
 
@@ -22,22 +27,33 @@ class EspressoMainActivityInstrumentationTests
 
     @Test
     fun testShowDateDialog(){
-        Espresso.onView(ViewMatchers.withId(R.id.btn_pick_range))
-            .perform(ViewActions.click())
+        Espresso.onView(withId(R.id.btn_pick_range))
+            .perform(click())
 
-        Espresso.onView(ViewMatchers.withText("Select")).inRoot(isDialog()).check(matches(isDisplayed()))
+        Espresso.onView(withText("Select")).inRoot(isDialog()).check(matches(isDisplayed()))
     }
 
     @Test
     fun testSelectInvalidRange() {
-        Espresso.onView(ViewMatchers.withId(R.id.btn_pick_range))
-            .perform(ViewActions.click())
+        Espresso.onView(withId(R.id.btn_pick_range))
+            .perform(click())
 
-        Espresso.onView(ViewMatchers.withText("Select"))
-            .perform(ViewActions.click())
+        Espresso.onView(withText("Select"))
+            .perform(click())
 
-        Espresso.onView(ViewMatchers.withText("A date range has not been selected"))
+        Espresso.onView(withText("A date range has not been selected"))
             .inRoot(withDecorView(not(`is`(activity.activity.window.decorView))))
             .check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun testAboutAppButton() {
+
+        Intents.init()
+        val expectedIntentkfechter = CoreMatchers.allOf(IntentMatchers.hasComponent(hasClassName(CalculendarAbout::class.java.name)))
+        Intents.intending(expectedIntentkfechter).respondWith(Instrumentation.ActivityResult(0, null))
+        Espresso.onView(withId(R.id.about_application)).perform(click())
+        Intents.intended(expectedIntentkfechter)
+        Intents.release()
     }
 }
